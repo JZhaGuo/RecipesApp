4df60d74d2b62bbf3da5fd5e3009a44f805b5595
JZhaGuo <91979940+JZhaGuo@users.noreply.github.com>
2022-03-30 09:55:43 +0200

    Update CodigoProyecto.py

--- CodigoProyecto.py
+++ CodigoProyecto.py
@@ -1,1 +1,70 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
+
+########################
+# Configurar la página #
+########################
+
+st.set_page_config(layout="wide", page_title="CookClick", 
+                   initial_sidebar_state="expanded", )  
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+###################
+# Leer data frame #
+###################
+
+st.title('Recetas')
+
+# df = pd.read_csv("/Users/jinhaozhangguo/Desktop/Proyi/RecetasReducido.csv")
+# st.dataframe(df)
+
+#########################
+# Seleccionar cada fila #
+#########################
+
+def aggrid_interactive_table(df: pd.DataFrame):
+
+    options = GridOptionsBuilder.from_dataframe(
+        receta, enableRowGroup=True, enableValue=True, enablePivot=True)
+
+    options.configure_side_bar()
+
+    options.configure_selection("single")
+    selection = AgGrid(df,
+        enable_enterprise_modules=True,
+        gridOptions=options.build(),
+        theme="dark",
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        allow_unsafe_jscode=True)
+
+    return selection
+
+receta = pd.read_csv("/Users/jinhaozhangguo/Desktop/Proyi/RecetasReducido.csv")
+
+selection = aggrid_interactive_table(df=receta)
+
+if selection:
+    st.write("You selected:")
+    st.json(selection["selected_rows"])
+
+#################
+# Barra lateral #
+#################
+
+st.sidebar.text("")
+st.sidebar.text("")
+provincia = st.sidebar.selectbox("Seleccione una categoría", ("Lleva más preparación", "Platos rápidos", "Postres",
+                                                            "(Próximamente)")) 
+
+
+
 
