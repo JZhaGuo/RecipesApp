9b83377a5a10ce685732cc99da315ee0460030d0
JZhaGuo <91979940+JZhaGuo@users.noreply.github.com>
2022-03-30 10:12:28 +0200

    Update CodigoProyecto.py

--- CodigoProyecto.py
+++ CodigoProyecto.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#from st_aggrid import AgGrid, GridOptionsBuilder
-#from st_aggrid.shared import GridUpdateMode
+from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid.shared import GridUpdateMode
 
 ########################
 # Configurar la página #
