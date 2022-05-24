d2c4511c6aaeaa41edd5414feb037d95808ec7d7
JZhaGuo <91979940+JZhaGuo@users.noreply.github.com>
2022-05-24 21:38:03 +0200

    Update CodigoProyecto.py

--- CodigoProyecto.py
+++ CodigoProyecto.py
@@ -40,9 +40,9 @@ st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 df = pd.read_csv("data.csv", delimiter=',') #.sample(n=4000, random_state=1)
 
-st.title('Recetas')
+st.title('Find what to cook')
 
-options = st.multiselect('Seleccione los ingredientes', ['sugar', 'brown sugar', 'milk', 'nuts', 'butter', 'beef', 'chicken breasts', 'sour cream', 
+options = st.multiselect('Select one or more ingredients:', ['sugar', 'brown sugar', 'milk', 'nuts', 'butter', 'beef', 'chicken breasts', 'sour cream', 
                        'cream cheese', 'salt', 'pepper', 'chicken', 'shredded cheese', 'powdered sugar', 'baking potatoes', 
                        'cheddar cheese', 'bacon', 'egg', 'eggs', 'buttermilk', 'flour', 'tomatoes', 'water', 'onions', 
                        'oil', 'pineapple', 'lemons', 'boiling water', 'barbacue sauce', 'ground beef', 'shredded lettuce', 
@@ -55,34 +55,43 @@ options = st.multiselect('Seleccione los ingredientes', ['sugar', 'brown sugar',
 cool1, cool2 = st.columns(2)
     
     
-filtros = cool1.selectbox('Ordenar por: ', ['Menor tiempo', 'Mayor tiempo', 'Menor número de pasos', 'Mayor número de pasos', 'Menor número de ingredientes', 'Mayor número de ingredientes'])
+filtros = cool1.selectbox('Select one filter: ', ['Less preparation time', 'More preparation time', 'Less steps', 'More steps', 'Less number of ingredients', 'More number of ingredients'])
 
-dif = cool2.selectbox('Elige dificultad: ',['Intermedia', 'Difícil', 'Fácil'])
+dif = cool2.selectbox('Select hardness: ',['Easy', 'Medium', 'Hard'])
 
+dificultad = {"Medium":'Intermedia', "Hard":'Difícil', "Easy":'Fácil'}
 
 if len(options) != 0:
-    with st.spinner("Buscando las recetas"):
+    with st.spinner("Searching recipes"):
         df = df.iloc[busquedaReceta(options)]
 
-    df = df.loc[df["Dificultad"]==dif]
-       
-    if filtros == 'Menor tiempo':
+    df = df.loc[df["Dificultad"]==dificultad[dif]]
+
+    if filtros == 'Less preparation time':
         df.sort_values(by=['Tiempo estimado'], ascending=True, inplace=True)
-    elif filtros == 'Mayor tiempo':
+    elif filtros == 'More preparation time':
         df.sort_values(by=['Tiempo estimado'], ascending=False, inplace=True)
-    elif filtros == 'Menor número de pasos':
-        df.sort_values(by=['Numero de pasos'], ascending=True, inplace=True)
-    elif filtros == 'Mayor número de pasos':
-        df.sort_values(by=['Numero de pasos'], ascending=False, inplace=True)
-    elif filtros == 'Menor número de ingredientes':
-        df.sort_values(by=['Lista Ingredientes'], ascending=True, inplace=True)
-    elif filtros == 'Mayor número de ingredientes':
-        df.sort_values(by=['Lista Ingredientes'], ascending=False, inplace=True)
+    elif filtros == 'Less steps':
+        df.sort_values(by=['Número de pasos'], ascending=True, inplace=True)
+    elif filtros == 'More steps':
+        df.sort_values(by=['Número de pasos'], ascending=False, inplace=True)
+    elif filtros == 'Less number of ingredients':
+        df.sort_values(by=['Número de ingredientes'], ascending=True, inplace=True)
+    elif filtros == 'More number of ingredients':
+        df.sort_values(by=['Número de ingredientes'], ascending=False, inplace=True)
         
     col1, col2 = st.columns(2)
     ing = df.iloc[:25]
-    res = col1.radio("Elige una receta para obtener más detalles", ing["title"])
+    res = col1.radio("Select a recipe for more information", ing["title"])
     ing2 = ing.loc[ing["title"] == res]
-    lista_ing= ", ".join(ast.literal_eval(list(ing2['ingredients2'])[0]))
-    lista_pasos= " ".join(ast.literal_eval(list(ing2['directions2'])[0]))
-    col2.markdown(f"#### Ingredientes\n{lista_ing}\n#### Instrucciones\n{lista_pasos}\n")
+    if True:
+        lista_ing= ", ".join(ast.literal_eval(list(ing2['ingredients2'])[0]))
+        lista_pasos= " ".join(ast.literal_eval(list(ing2['directions2'])[0]))
+        col2.markdown(f"#### Ingredients\n{lista_ing}\n#### Instructions\n{lista_pasos}\n\n##### Time: {int(ing2['Tiempo estimado'])} minutes")
+
+
+    
+    
+    
+    
+    
